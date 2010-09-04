e5e7ee983bad2c143c75727697061ba646fc8cd1
Brian Goslinga <gosli008@morris.umn.edu>
2010-09-04 12:18:39 -0500

    Added chunking to mapreduce

--- src/mr/core.clj
+++ src/mr/core.clj
@@ -1,37 +1,51 @@
 (ns mr.core
   (:require [mr.wq :as wq]))
 
-(defn- mr-worker
-  "Helper fuction for mapreduce.  wq is the work queue, cont is the
-continuation, f is the map fn, g is the reduce fn, init is the identity of g,
-and vec is a vector"
-  [wq cont f g init vec]
+(defn- mr-single
+  "mapreduce vec on this thread"
+  [cont f g init vec]
   (let [n (count vec)]
-    (cond
-     (zero? n) (cont init)
-     (= n 1) (cont (f (nth vec 0)))
-     :else (let [half (quot n 2)
-		 left (subvec vec 0 half)
-		 right (subvec vec half (count vec))]
-	     (wq/invoke-later-task
-	      wq
-	      #(cont (g %1 %2))
-	      #(mr-worker wq % f g init left)
-	      #(mr-worker wq % f g init right))))))
+    (if (zero? n)
+      (cont init)
+      (loop [pos 0, acc init]
+	(if (< pos n)
+	  (recur (inc pos) (g acc (f (nth vec pos))))
+	  (cont acc))))))
+
+(defn- mr-worker
+  "Helper fuction for mapreduce.  wq is the work queue, t is the threshold, cont
+is the continuation, f is the map fn, g is the reduce fn, init is the identity
+of g, and vec is a vector"
+  [wq t cont f g init vec]
+  (let [n (count vec)]
+    (if (<= n t)
+      (mr-single cont f g init vec)
+      (let [half (quot n 2)
+	    left (subvec vec 0 half)
+	    right (subvec vec half (count vec))]
+	(wq/invoke-later-task
+	 wq
+	 #(cont (g %1 %2))
+	 #(mr-worker wq t % f g init left)
+	 #(mr-worker wq t % f g init right))))))
 			   
 (defn mapreduce
-  "Mapreduce coll (vector preferred) using n workers with the mapping fn being
-f, the reducing fn being g, and the identity of g being init.  Only useful when
-the cost of mapping and reducing outweighs the coordination costs of executing
-in parallel."
-  [n f g init coll]
+  "Mapreduce coll (vector preferred), where f is the mapper, g is the reducer,
+and init is the identity of g.  mapreduce works in parallel, with n worker
+threads, each processing chunks up to c elements of coll long."
+  [n c f g init coll]
   (if (not (vector? coll))
-    (recur n f g init (vec coll))
+    (recur n c f g init (vec coll))
     (let [wq (wq/make-wq n)
 	  val (promise)
 	  final (fn [v]
 		  (deliver val v)
 		  (wq/shutdown-wq wq))]
-      (wq/add-task wq #(mr-worker wq final f g init coll))
+      (wq/add-task wq #(mr-worker wq c final f g init coll))
       @val)))
 
+(defn mr
+  "Mapreduce factory function; returns mapreduce with the first two arguments
+provided, the number of workers and the size of the chunks"
+  [n c]
+  #(mapreduce n c %1 %2 %3 %4))
